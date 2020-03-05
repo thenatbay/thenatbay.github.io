d14c87f66c22579d19234bba07bafc3c60475ae9
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-03-06 09:18:45 +1000

    update

--- js/login.js
+++ js/login.js
@@ -1,37 +1,55 @@
 // Loop through Array of Objects
 var objPeople = [
 	{ // Object @ 0 index
-		id: "admin", // charlie
+		id: "100032730001215739040", // charliekmcmahon
 	},
 	{ // Object @ 1 index
-		id: "", //sam
+		id: "112983193748391848319", //fake
 	},
 	{ // Object @ 2 index
-		id: ""
+		id: "137887463718764731876" //fake
 	}
 
 ]
 
+var userID = "null"
+
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	userID = profile.getId();
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+  var friendlyName = profile.getName()
+	getInfo()
+  //console.log(friendlyName+ " is logged in!!!");
+  //document.getElementById("output_label").style.display = "block";
+  //document.getElementById("output_label").style.color = "#77dd77";
+  //document.getElementById('output_label').innerHTML = "Successfully logged in. Redirecting.";
+  //setTimeout(function(){window.location.replace("./main.html");}, 2050);
+  //window.location.replace("./main.html");
+}
+
 function getInfo() {
-	var username = document.getElementById('username').value
-	var password = document.getElementById('password').value
 
 	for(var i = 0; i < objPeople.length; i++) {
 		// check is user input matches username and password of a current index of the objPeople array
-		if(username == objPeople[i].username && password == objPeople[i].password) {
+		if(userID == objPeople[i].userID) {
 			console.log(username + " is logged in!!!");
       document.getElementById("output_label").style.display = "block";
       document.getElementById("output_label").style.color = "#77dd77";
       document.getElementById('output_label').innerHTML = "Successfully logged in. Redirecting.";
-      //setTimeout(function(){window.location.replace("./main.html");}, 1250); //// FIXME: 
-      //
-			// stop the function if this is found to be true
+      setTimeout(function(){window.location.replace("./main.html");}, 1250);
 			return
 		}
 	}
   document.getElementById("output_label").style.display = "block";
   document.getElementById("output_label").style.color = "#ff6961";
-  document.getElementById('output_label').innerHTML = "Incorrect username or password.";
-	console.log("incorrect username or password")
+  document.getElementById('output_label').innerHTML = "You are not authorised to access Nateflix.";
+	console.log("not-authorised")
 }
 document.getElementById("output_label").style.display = "none";
