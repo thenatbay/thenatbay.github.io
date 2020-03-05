33f7ead06fb7ce5e66299f8c76de083d3eee01f7
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-03-05 21:50:50 +1000

    update

--- js/login.js
+++ js/login.js
@@ -14,6 +14,17 @@ var objPeople = [
 	}
 
 ]
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	document.getElementById("output_label").style.display = "block";
+	document.getElementById("output_label").style.color = "#77dd77";
+	document.getElementById('output_label').innerHTML = "Successfully logged in. Redirecting.";
+	setTimeout(function(){window.location.replace("./main.html");}, 1250);
+}
 
 function getInfo() {
 	var username = document.getElementById('username').value
